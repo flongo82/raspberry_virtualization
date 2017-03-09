7cde94347d9bbb1abfc751354a1a25a38d1cb6e9
Francesco Longo <flongo@unime.it>
2017-03-09 12:20:14 +0100

    Update launch_virtual_rasp.sh

--- launch_virtual_rasp.sh
+++ launch_virtual_rasp.sh
@@ -10,18 +10,18 @@ echo "Creating virtual rasp "$name"!"
 
 lxc launch ubuntu:16.04 $name
 
-eval UID_"$name"=`sudo ls -l /var/lib/lxd/containers/"$name"/rootfs/ | grep root | awk '{}{print $3}{}'`
+MYUID=`sudo ls -l /var/lib/lxd/containers/"$name"/rootfs/ | grep root | awk '{}{print $3}{}'`
 
 lxc exec "$name" -- addgroup gpio
 lxc exec "$name" -- usermod -a -G gpio ubuntu
-eval GID_"$name"=$(($UID_"$name" + `lxc exec "$name" -- sed -nr "s/^gpio:x:([0-9]+):.*/\1/p" /etc/group`))
+MYGID=$(($MYUID + `lxc exec "$name" -- sed -nr "s/^gpio:x:([0-9]+):.*/\1/p" /etc/group`))
 
 sudo mkdir -p /gpio_mnt/"$name"
 sudo chmod 777 -R /gpio_mnt/
 
 sudo mkdir -p /gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio
 sudo mkdir -p /gpio_mnt/"$name"/sys/class/gpio
-sudo chown "$UID_"$name""."$GID_"$name"" -R /gpio_mnt/"$name"/sys/
+sudo chown "$MYUID"."$MYGID" -R /gpio_mnt/"$name"/sys/
 
 lxc exec "$name" -- mkdir -p /gpio_mnt/sys/class/gpio
 lxc exec "$name" -- mkdir -p /gpio_mnt/sys/devices/platform/soc/3f200000.gpio
@@ -30,6 +30,6 @@ lxc config device add "$name" gpio disk source=/gpio_mnt/"$name"/sys/class/gpio 
 lxc config device add "$name" devices disk source=/gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio path=/gpio_mnt/sys/devices/platform/soc/3f200000.gpio
 
 cd /home/ubuntu/test_gpio_mirroring/
-sudo node node-folder-mirroring.js /sys/devices/platform/soc/3f200000.gpio /gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio -o uid=$UID_"$name" -o gid=$GID_"$name" -o allow_other &> log_devices_"$name" &
-sudo node node-folder-mirroring.js /sys/class/gpio /gpio_mnt/"$name"/sys/class/gpio -o uid=$UID_"$name" -o gid=$GID_"$name" -o allow_other &> log_gpio_"$name" &
+sudo node node-folder-mirroring.js /sys/devices/platform/soc/3f200000.gpio /gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio -o uid=$MYUID -o gid=$MYGID -o allow_other &> log_devices_"$name" &
+sudo node node-folder-mirroring.js /sys/class/gpio /gpio_mnt/"$name"/sys/class/gpio -o uid=$MYUID -o gid=$MYGID -o allow_other &> log_gpio_"$name" &
 
