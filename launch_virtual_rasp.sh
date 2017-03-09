516858032f89df707e824b0ecb52442456b562db
Francesco Longo <flongo@unime.it>
2017-03-09 09:48:42 +0100

    launch_virtual_rasp.sh script - to be tested

---
+++ launch_virtual_rasp.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: launch_virtual_rasp.sh <virtual_rasp_name>"
+    exit;
+fi
+
+$name = $1
+
+lxc launch ubuntu:16.04 $name
+
+UID_"$name"=`sudo ls -l /var/lib/lxd/containers/"$name"/rootfs/ | grep root | awk '{}{print $3}{}'`
+
+lxc exec "$name" -- addgroup gpio
+lxc exec "$name" -- usermod -a -G gpio ubuntu
+GID_"$name"=$(($UID_"$name" + `lxc exec "$name" -- sed -nr "s/^gpio:x:([0-9]+):.*/\1/p" /etc/group`))
+
+sudo mkdir -p /gpio_mnt/"$name"
+sudo chmod 777 -R /gpio_mnt/
+
+sudo mkdir -p /gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio
+sudo mkdir -p /gpio_mnt/"$name"/sys/class/gpio
+sudo chown "$UID_"$name""."$GID_"$name"" -R /gpio_mnt/"$name"/sys/
+
+lxc exec "$name" -- mkdir -p /gpio_mnt/sys/class/gpio
+lxc exec "$name" -- mkdir -p /gpio_mnt/sys/devices/platform/soc/3f200000.gpio
+
+lxc config device add "$name" gpio disk source=/gpio_mnt/"$name"/sys/class/gpio path=/gpio_mnt/sys/class/gpio
+lxc config device add "$name" devices disk source=/gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio path=/gpio_mnt/sys/devices/platform/soc/3f200000.gpio
+
+cd test_gpio_mirroring/
+sudo node node-folder-mirroring.js /sys/devices/platform/soc/3f200000.gpio /gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio -o uid=$UID_"$name" -o gid=$GID_"$name" -o allow_other &> log_devices_"$name" &
+sudo node node-folder-mirroring.js /sys/class/gpio /gpio_mnt/"$name"/sys/class/gpio -o uid=$UID_"$name" -o gid=$GID_"$name" -o allow_other &> log_gpio_"$name" &
+
