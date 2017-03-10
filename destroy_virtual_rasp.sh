a9265d6249ae08df49f48764f66abc041f82cc32
Francesco Longo <flongo@unime.it>
2017-03-10 09:32:05 +0100

    Create destroy_virtual_rasp.sh

---
+++ destroy_virtual_rasp.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+if [ "$#" -ne 1 ]; then
+    echo "Usage: destroy_virtual_rasp.sh <virtual_rasp_name>"
+    exit;
+fi
+
+name=$1
+echo "Destroying virtual rasp "$name"!"
+
+sudo kill -9 `cat /home/ubuntu/test_gpio_mirroring/log_devices_pid_"$name"`
+sudo kill -9 `cat /home/ubuntu/test_gpio_mirroring/log_gpio_pid_"$name"`
+sudo rm log_*_"$name"
+
+sudo umount /gpio_mnt/"$name"/sys/devices/platform/soc/3f200000.gpio
+sudo umount /gpio_mnt/"$name"/sys/class/gpio
+
+lxc config device remove "$name" gpio disk 
+lxc config device remove "$name" devices disk
+
+sudo rm -rf /gpio_mnt/"$name"
+
+lxc delete --force $name
